04b64403a899cf70d37cf9689ef81e46ee29adf7
Peter Phan <peter_phan05@yahoo.com>
2021-05-10 02:17:25 -0700

    Detail Button and Description

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -7,9 +7,32 @@ function Detail() {
             <Background>
                 <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/4F39B7E16726ECF419DD7C49E011DD95099AA20A962B0B10AA1881A70661CE45/scale?width=1440&aspectRatio=1.78&format=jpeg" />
             </Background>
-            <Title>
+            <ImageTitle>
                 <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/D7AEE1F05D10FC37C873176AAA26F777FC1B71E7A6563F36C6B1B497CAB1CEC2/scale?width=1440&aspectRatio=1.78" />
-            </Title>
+            </ImageTitle>
+            <Controls>
+                <PlayButton>
+                    <img src="/images/play-icon-black.png" />
+                    <span>PLAY</span>
+                </PlayButton>
+                <TrailerButton>
+                    <img src="/images/play-icon-white.png" />
+                    <span>Trailer</span>
+                </TrailerButton>
+                <AddButton>
+                    <span>+</span>
+                </AddButton>
+                <GroupWatchButton>
+                    <img src="/images/group-icon.png" />
+                </GroupWatchButton>
+            </Controls>
+            <SubTitle>2018 7m Family, Fantasy, Kids, Animation</SubTitle>
+            <Description>
+                l jlkj sdlfjsdljfosid ijdf ldjf sdjfo iid di fid i diofdj.
+                dfjlksdjflksjd;f
+                dskfjsldjflsdj
+                sdjlfksjdlfjslkdfjlksjdflsjdlfkj00000000000000000
+            </Description>
         </Container>
     );
 }
@@ -38,7 +61,7 @@ const Background = styled.div`
     }
 `;
 
-const Title = styled.div`
+const ImageTitle = styled.div`
     height: 30vh;
     min-height: 170px;
     width: 35vw;
@@ -49,4 +72,69 @@ const Title = styled.div`
         height: 100%;
         object-fit: contain;
     }
-`
+`;
+const Controls = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const PlayButton = styled.button`
+    border-radius: 4px;
+    font-size: 15px;
+    padding: 0px 24px;
+    margin-right: 22px;
+    display: flex;
+    align-items: center;
+    height: 56px;
+    background: rgb(249, 249, 249);
+    border: none;
+    letter-spacing: 1.8px;
+    cursor: pointer;
+
+    &:hover {
+        background: rgb(198, 198, 198);
+    }
+`;
+
+const TrailerButton = styled(PlayButton)`
+    background: rgba(0, 0, 0, 0.3);
+    border: 1px solid rgb(249, 249, 249);
+    color: rgb(249, 249, 249);
+    text-transform: uppercase;
+`;
+
+const AddButton = styled.button`
+    margin-right: 16px;
+    width: 44px;
+    height: 44px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    border: 2px solid white;
+    background-color: rgba(0, 0, 0, 0.6);
+    cursor: pointer;
+
+    span {
+        font-size: 30px;
+        color: white;
+    }
+`;
+
+const GroupWatchButton = styled(AddButton)`
+    background: rgb(0, 0, 0);
+`;
+
+const SubTitle = styled.div`
+    color: rgb(249, 249, 249);
+    font-size: 15px;
+    min-height: 20px;
+    margin-top: 26px;
+`;
+
+const Description = styled.div`
+    line-height: 1.4;
+    font-size: 20px;
+    margin-top: 16px;
+    color: (249, 249, 249);
+`;
